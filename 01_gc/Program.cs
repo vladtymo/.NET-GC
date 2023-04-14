80bece484bb7ced8081255ce5be31af28482e098
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-04-14 16:19:06 +0300

    Add project files.

---
+++ 01_gc/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+namespace SimpleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            WriteLine("Демонстрация System.GC");
+            WriteLine($"Максимальное поколение: { GC.MaxGeneration}");
+
+            GarbageHelper hlp = new GarbageHelper();
+
+            //узнаем поколение, в котором находится объект
+            WriteLine($"Поколение объекта:{ GC.GetGeneration(hlp)}");
+
+            // количество занятой памяти
+            WriteLine($"Занято памяти(байт):{ GC.GetTotalMemory(false)}");
+            hlp.MakeGarbage(); //создаем мусор
+            WriteLine($"Занято памяти(байт):{ GC.GetTotalMemory(false)}");
+
+            Console.ReadKey();
+            GC.Collect(0); //вызываем явный сбор мусора в поколении 0
+
+            WriteLine($"Занято памяти(байт):{ GC.GetTotalMemory(false)}");
+            WriteLine($"Поколение объекта:{ GC.GetGeneration(hlp)}");
+
+            Console.ReadKey();
+            GC.Collect(); //вызываем явный сбор мусора во всех поколениях
+
+            WriteLine($"Занято памяти(байт):{ GC.GetTotalMemory(false)}");
+            WriteLine($"Поколение объекта:{ GC.GetGeneration(hlp)}");
+        }
+    }
+
+    //Вспомогательный класс для создания мусора
+    class GarbageHelper
+    {
+        //Метод, создающий мусор
+        public void MakeGarbage()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Person p = new Person();
+            }
+        }
+        class Person
+        {
+            string _name;
+            decimal _salary;
+            byte _age;
+        }
+    }
+}
---
+++ 02_dispose/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_dispose
+{
+    class HardClass : IDisposable
+    {
+        int[] array = null;
+        bool isConnected = false;
+        private bool disposedValue;
+
+        public HardClass()
+        {
+            Connect();
+            array = new int[1000];
+        }
+        public void Connect()
+        {
+            isConnected = true;
+            Console.WriteLine("Connected!");
+        }
+        public void Disconnect()
+        {
+            isConnected = false;
+            Console.WriteLine("Disconnected!");
+        }
+        public void BadOperation()
+        {
+            throw new Exception("Error!");
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                Disconnect();
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~HardClass()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+    class Program
+    {
+        static void TestHardWork()
+        {
+            //HardClass hard = new HardClass();
+            //try
+            //{
+            //    // ... working with object ...
+            //    hard.BadOperation();
+            //}
+            //finally
+            //{
+            //    // clear unmanagement resources
+            //    hard.Disconnect();
+            //}
+
+            //................ the same
+            using (HardClass hard = new HardClass()) // create an instance of IDisposable class
+            {
+                // ... working with object ...
+                hard.BadOperation();
+
+            } // invoke Dispose() method
+        }
+        static void Main(string[] args)
+        {
+            TestHardWork();
+
+            GC.Collect();
+            Console.ReadKey();
+        }
+    }
+}
